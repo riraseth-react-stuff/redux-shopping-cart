58e4605d71b5b3926719350be515d84d64ccd247
Łukasz Zabuski <riraseth@gmail.com>
2020-04-03 23:12:14 +0200

    add: increase, decrease amount

--- src/reducer.js
+++ src/reducer.js
@@ -3,9 +3,30 @@ import { INCREASE, DECREASE, CLEAR_CART, REMOVE } from './actions';
 export const reducer = (state, action) => {
   switch (action.type) {
     case DECREASE:
-      console.log('increase');
+      if (action.payload.amount === 1) {
+        return {
+          ...state,
+          cart: state.cart.filter((item) => item.id !== action.payload.id),
+        };
+      } else {
+        return {
+          ...state,
+          cart: state.cart.map((item) => {
+            return item.id === action.payload.id
+              ? { ...item, amount: item.amount - 1 }
+              : { ...item };
+          }),
+        };
+      }
     case INCREASE:
-      console.log('decrease');
+      return {
+        ...state,
+        cart: state.cart.map((item) => {
+          return item.id === action.payload.id
+            ? { ...item, amount: item.amount + 1 }
+            : { ...item };
+        }),
+      };
     case CLEAR_CART:
       return { ...state, cart: [] };
     case REMOVE:
