6b5afc0c0ed6012887790363913fda0176eff3ab
Łukasz Zabuski <riraseth@gmail.com>
2020-04-04 18:18:37 +0200

    add: get totals

--- src/reducer.js
+++ src/reducer.js
@@ -1,23 +1,23 @@
-import { INCREASE, DECREASE, CLEAR_CART, REMOVE } from './actions';
+import { INCREASE, DECREASE, CLEAR_CART, REMOVE, GET_TOTALS } from './actions';
 
 export const reducer = (state, action) => {
   switch (action.type) {
     case DECREASE:
-      if (action.payload.amount === 1) {
-        return {
-          ...state,
-          cart: state.cart.filter((item) => item.id !== action.payload.id),
-        };
-      } else {
-        return {
-          ...state,
-          cart: state.cart.map((item) => {
-            return item.id === action.payload.id
-              ? { ...item, amount: item.amount - 1 }
-              : { ...item };
-          }),
-        };
-      }
+      // if (action.payload.amount === 1) {
+      //   return {
+      //     ...state,
+      //     cart: state.cart.filter((item) => item.id !== action.payload.id),
+      //   };
+      // } else {
+      return {
+        ...state,
+        cart: state.cart.map((item) => {
+          return item.id === action.payload.id
+            ? { ...item, amount: item.amount - 1 }
+            : { ...item };
+        }),
+        // };
+      };
     case INCREASE:
       return {
         ...state,
@@ -34,6 +34,22 @@ export const reducer = (state, action) => {
         ...state,
         cart: state.cart.filter((item) => item.id !== action.payload.id),
       };
+    case GET_TOTALS:
+      let { total, amount } = state.cart.reduce(
+        (cartTotal, cartItem) => {
+          const { price, amount } = cartItem;
+          const itemTotal = price * amount;
+          cartTotal.total += itemTotal;
+          cartTotal.amount += amount;
+          return cartTotal;
+        },
+        {
+          total: 0,
+          amount: 0,
+        }
+      );
+      total = parseFloat(total.toFixed(2));
+      return { ...state, total, amount };
     default:
       return state;
   }
