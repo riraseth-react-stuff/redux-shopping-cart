1c14067fad631251a1309b67714bab09ef432adc
Łukasz Zabuski <riraseth@gmail.com>
2020-04-04 18:48:05 +0200

    reducer refactor

--- src/reducer.js
+++ src/reducer.js
@@ -1,23 +1,31 @@
-import { INCREASE, DECREASE, CLEAR_CART, REMOVE, GET_TOTALS } from './actions';
+import {
+  INCREASE,
+  DECREASE,
+  CLEAR_CART,
+  REMOVE,
+  GET_TOTALS,
+  TOGGLE_AMOUNT,
+} from './actions';
 
 export const reducer = (state, action) => {
   switch (action.type) {
+    // not used //
     case DECREASE:
-      // if (action.payload.amount === 1) {
-      //   return {
-      //     ...state,
-      //     cart: state.cart.filter((item) => item.id !== action.payload.id),
-      //   };
-      // } else {
-      return {
-        ...state,
-        cart: state.cart.map((item) => {
-          return item.id === action.payload.id
-            ? { ...item, amount: item.amount - 1 }
-            : { ...item };
-        }),
-        // };
-      };
+      if (action.payload.amount === 1) {
+        return {
+          ...state,
+          cart: state.cart.filter((item) => item.id !== action.payload.id),
+        };
+      } else {
+        return {
+          ...state,
+          cart: state.cart.map((item) => {
+            return item.id === action.payload.id
+              ? { ...item, amount: item.amount - 1 }
+              : { ...item };
+          }),
+        };
+      }
     case INCREASE:
       return {
         ...state,
@@ -27,6 +35,8 @@ export const reducer = (state, action) => {
             : { ...item };
         }),
       };
+    /////////////////
+
     case CLEAR_CART:
       return { ...state, cart: [] };
     case REMOVE:
@@ -50,6 +60,17 @@ export const reducer = (state, action) => {
       );
       total = parseFloat(total.toFixed(2));
       return { ...state, total, amount };
+    case TOGGLE_AMOUNT:
+      return {
+        ...state,
+        cart: state.cart.map((item) => {
+          return item.id === action.payload.id
+            ? action.payload.toggle === 'inc'
+              ? { ...item, amount: item.amount + 1 }
+              : { ...item, amount: item.amount - 1 }
+            : { ...item };
+        }),
+      };
     default:
       return state;
   }
